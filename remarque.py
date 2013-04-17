5f1622836695fb2a5db71919b3ba3bf919012227
AquarHEAD L <aquarhead@gmail.com>
2013-04-17 19:25:49 +0800

    user-biased notes

--- remarque.py
+++ remarque.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_peewee.db import Database
 from flask_peewee.auth import Auth, BaseUser
 from flask_peewee.admin import Admin, ModelAdmin
-from flask_peewee.rest import RestAPI, RestResource, RestrictOwnerResource, UserAuthentication
+from flask_peewee.rest import RestAPI, RestResource, RestrictOwnerResource, UserAuthentication, AdminAuthentication
 from peewee import CharField, TextField, DateTimeField, BooleanField, ForeignKeyField
 import times
 
@@ -12,16 +12,16 @@ app.config.from_pyfile("config.cfg")
 db = Database(app)
 auth = Auth(app, db)
 
-# class User(db.Model, BaseUser):
-#     username = CharField()
-#     password = CharField()
-#     email = CharField()
-#     join_date = DateTimeField(default=times.now)
-#     active = BooleanField(default=True)
-#     admin = BooleanField(default=False)
+class User(db.Model, BaseUser):
+    username = CharField()
+    password = CharField()
+    email = CharField()
+    join_date = DateTimeField(default=times.now)
+    active = BooleanField(default=True)
+    admin = BooleanField(default=False)
 
 class Note(db.Model):
-    # user = ForeignKeyField(User)
+    user = ForeignKeyField(User)
     title = CharField()
     content = TextField()
     created = DateTimeField(default=times.now)
@@ -34,23 +34,19 @@ admin.register(Note, NoteAdmin)
 auth.register_admin(admin)
 admin.setup()
 
-# class UserResource(RestResource):
-#     exclude = ('password', 'email',)
+class UserResource(RestResource):
+    exclude = ('password', 'email',)
 
-# class NoteResource(RestrictOwnerResource):
-#     owner_field = 'user'
+class NoteResource(RestrictOwnerResource):
+    owner_field = 'user'
 
 user_auth = UserAuthentication(auth)
-# api = RestAPI(app, default_auth=user_auth)
-# api.register(User, UserResource)
-# api.register(Note, NoteResource)
-# api.setup()
-# admin_auth = AdminAuthentication(auth)
+admin_auth = AdminAuthentication(auth)
 
 api = RestAPI(app, default_auth=user_auth)
-api.register(Note)
+api.register(User, UserResource, auth=admin_auth)
+api.register(Note, NoteResource)
 api.setup()
 
 if __name__ == '__main__':
-    # Note.create_table(fail_silently=True)
     app.run(host="0.0.0.0", port=app.config["PORT"])
